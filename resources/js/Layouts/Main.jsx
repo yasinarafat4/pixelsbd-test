c58fa76350d8eec2bdf28a036c449bc627d8aa28
yasinarafat4 <mdyasinarafathasib@gmail.com>
2024-03-02 17:57:06 +0600

    Navbar almost done

--- resources/js/Layouts/Main.jsx
+++ resources/js/Layouts/Main.jsx
@@ -4,9 +4,11 @@ import Navbar from "@/Pages/Partial/Navbar";
 export default function Main({ children }) {
     return (
         <>
-            <div className="max-w-[95rem] mx-auto">
+            <div>
                 <Navbar />
-                {children}
+                <div className="min-h-[calc(100vh-330px)]">
+                    {children}
+                </div>
             </div>
             <Footer />
         </>)
--- resources/js/Pages/Partial/Footer.jsx
+++ resources/js/Pages/Partial/Footer.jsx
@@ -1,5 +1,6 @@
 export default function Footer() {
-    return (<div>
-       Footer
+    return (<div
+    className="bg-slate-800 text-slate-200 px-40 py-20">
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis ex assumenda a eius nisi consequatur quas ut error. Itaque, sunt dolor debitis vel placeat quae ullam corporis officia nihil saepe sequi magnam! Voluptate in quae iste qui tempora beatae quia optio unde facilis ipsum repellat ab, nesciunt, vitae laudantium quos suscipit doloribus a numquam temporibus cumque. Quae eligendi asperiores soluta nemo qui eos modi molestias atque ullam reiciendis, exercitationem neque? Eveniet aliquam quisquam repudiandae nam! Numquam modi tenetur reprehenderit illum provident enim hic dicta iusto incidunt adipisci quasi unde pariatur et facere dignissimos autem porro, deserunt veritatis placeat vero atque?
     </div>)
 }
